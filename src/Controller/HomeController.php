d708fcf71a5d062cc8c6916e44803297f18b904b
greeflas <vldmr.kuprienko@gmail.com>
2020-08-27 21:15:33 +0300

    Add basic layout and styles for home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,16 +4,17 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class HomeController
+final class HomeController extends AbstractController
 {
     /**
      * @Route("/", methods={"GET"}, name="app_home")
      */
     public function index(): Response
     {
-        return new Response('<h1>Hello from Symfony app!</h1>');
+        return $this->render('home/index.html.twig');
     }
 }
